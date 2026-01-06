53f5c43e3a35e54f615d64e552429cbea9878041
Naleen Dissanayake <naleenituom@gmail.com>
2026-01-06 17:27:58 +0900

    Initial commit #1 - Project Setup

---
+++ SwiftUI-Practice/SwiftUI-Practice/SwiftUI_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PracticeApp.swift
+//  SwiftUI-Practice
+//
+//  Created by Naleen Dissanayake on 2026/01/06.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
